45285e57258c51decd6287e983457e64a8a828e9
Afiq Razak <120654244+afiqrazak380@users.noreply.github.com>
2023-10-22 11:13:01 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const myButton = document.querySelector("#myButton");
+const myImg = document.querySelector("#myImg");
+
+// myButton.addEventListener("click", () => {
+
+//   if(myImg.style.display == "none"){
+//     myImg.style.display = "block";
+//   }
+//   else{
+//     myImg.style.display = "none";
+//   }
+
+// })
+
+myButton.addEventListener("click", () => {
+
+  if(myImg.style.visibility == "hidden"){
+    myImg.style.visibility = "visible";
+  }
+  else{
+    myImg.style.visibility = "hidden";
+  }
+
+})
